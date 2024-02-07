4621d1131b4e7a9f007c4ec4a5393fa3e07b1eaa
Aryan7704 <aryans7704@gmail.com>
2024-02-07 18:43:04 +0530

    3rd commit of the project

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const platformImg = new Image()
 platformImg.src = './img/platform.png'
 
+const winflagImg = new Image()
+winflagImg.src = './img/winflag.png'
+
 
 const smallplatformImg = new Image()
 smallplatformImg.src = './img/platformSmallTall.png'
@@ -82,6 +85,25 @@ class Background {
 }
 
 
+// creation of the win flag
+class Winflag {
+    constructor(x, y, img) {
+        this.position = {
+            x,
+            y
+        }
+        this.image = img
+        this.width = 200
+        this.height = 300
+    }
+
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+
 // creation of the hills class
 class Hills {
     constructor(x, y, img) {
@@ -116,8 +138,23 @@ class Platform {
         c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
+// creation of smallplatform class
+class Smallplatform {
+    constructor(x, y, img) {
+        this.position = {
+            x,
+            y
+        }
+        this.image = img
+        this.width = smallplatformImg.width
+        this.height = smallplatformImg.height
+    }
 
 
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
 
 
 // creation of player
@@ -127,6 +164,10 @@ let player = new Player()
 let backgroundImg = new Background(-1, -1, bgImg)
 
 
+// creation of background
+let winflag = new Winflag(11000, 165, winflagImg)
+
+
 // creation of hills
 let hillImg = new Background(-1, -1, hills)
 
@@ -134,10 +175,27 @@ let hillImg = new Background(-1, -1, hills)
 let platforms = [new Platform(-1, 455, platformImg),
 new Platform(platformImg.width * 2 - 580 - 3, 455, platformImg),
 new Platform(platformImg.width * 3 - 580 + 150, 455, platformImg),
+new Platform(platformImg.width * 4 - 580 + 150, 375, platformImg),
+new Platform(platformImg.width * 6 - 300, 455, platformImg),
+new Platform(platformImg.width * 7 - 302, 455, platformImg),
+new Platform(platformImg.width * 9 - 302, 455, platformImg),
+new Platform(platformImg.width * 10 - 100, 500, platformImg),
+new Platform(platformImg.width * 13, 455, platformImg),
+new Platform(platformImg.width * 14 + 200, 455, platformImg),
+new Platform(platformImg.width * 18 + 150, 455, platformImg),
+new Platform(platformImg.width * 19 - 3 + 150, 455, platformImg),
+new Platform(platformImg.width * 20 - 5 + 150, 455, platformImg),
 ]
 
-
-
+// creation of the smallplatforms [smallplatform array]
+let smallplatforms = [new Smallplatform(platformImg.width * 5 - 580 + 150, 200, smallplatformImg),
+new Smallplatform(platformImg.width * 7 - 302, 175, smallplatformImg),
+new Smallplatform(platformImg.width * 8 - 302, 100, smallplatformImg),
+new Smallplatform(platformImg.width * 11, 100, smallplatformImg),
+new Smallplatform(platformImg.width * 12, 200, smallplatformImg),
+new Smallplatform(platformImg.width * 16 - 100, 200, smallplatformImg),
+new Smallplatform(platformImg.width * 17, 200, smallplatformImg),
+]
 
 const keys = {
     right: {
@@ -155,17 +213,20 @@ let scrolloffset = 0
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
+
     backgroundImg.draw()
-
     hillImg.draw()
-
+    winflag.draw()
     player.update()
 
-    
-   
     platforms.forEach((platform) => {
         platform.draw()
     })
+    smallplatforms.forEach((smallplatform) => {
+        smallplatform.draw()
+    })
+
+
 
 
     if (keys.right.pressed && player.position.x < 400) {
@@ -177,14 +238,17 @@ function animate() {
     else {
         player.velocity.x = 0
 
-        if (keys.right.pressed) {
+        if (keys.right.pressed && scrolloffset < 11050) {
             scrolloffset += player.speed
             platforms.forEach((platform) => {
                 platform.position.x -= player.speed
             })
+            smallplatforms.forEach((smallplatform) => {
+                smallplatform.position.x -= player.speed
+            })
             backgroundImg.position.x -= 3
             hillImg.position.x -= 4
-
+            winflag.position.x -= player.speed
 
 
         }
@@ -194,8 +258,12 @@ function animate() {
             platforms.forEach((platform) => {
                 platform.position.x += player.speed
             })
+            smallplatforms.forEach((smallplatform) => {
+                smallplatform.position.x += player.speed
+            })
             backgroundImg.position.x += 3
             hillImg.position.x += 4
+            winflag.position.x += player.speed
 
         }
     }
@@ -204,6 +272,13 @@ function animate() {
     // platform collision detection
     platforms.forEach((platform) => {
         if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+
+    })
+    // samllplatform collision detection
+    smallplatforms.forEach((smallplatform) => {
+        if (player.position.y + player.height <= smallplatform.position.y && player.position.y + player.height + player.velocity.y >= smallplatform.position.y && player.position.x + player.width >= smallplatform.position.x && player.position.x <= smallplatform.position.x + smallplatform.width) {
             player.velocity.y = 0
         }
 
@@ -218,10 +293,10 @@ function animate() {
     // loose scenario
     if (player.position.y > canvas.height) {
         console.log('you loose')
-        
+
     }
 
-
+    console.log(scrolloffset)
 }
 
 
